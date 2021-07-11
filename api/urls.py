6c893a2b5a8aa70fca9d9a5ffe00bc432b268861
Kubson900 <polczykj@gmail.com>
2021-07-11 21:45:46 +0200

    Added entry point to API and pagination

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.api_root),
     path('contacts', views.ContactList.as_view(), name='contact-list'),
     path('contacts/<int:pk>', views.ContactDetail.as_view(), name='contact-detail'),
     path('users', views.UserList.as_view(), name='user-list'),
--- api/views.py
+++ api/views.py
@@ -1,7 +1,10 @@
 from address_book.models import Contact
 from django.contrib.auth.models import User
 from .serializers import ContactSerializer, UserSerializer
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, renderers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 from .permissions import IsOwnerOrReadOnly
 
 
@@ -28,3 +31,11 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'contacts': reverse('contact-list', request=request, format=format)
+    })
